86677243dea4418dd7f36604cec4c93033093053
Janashell <janashell07@gmail.com>
2021-03-09 10:19:48 -0500

    HTMl file and JS file that is not completed

---
+++ task.js
@@ -0,0 +1,1 @@
+// I literally have nothing because everthing that I did try did not work, so i just got frustrated and got rid of what little bit I did have.  All i had were some variable names after hours of trying
